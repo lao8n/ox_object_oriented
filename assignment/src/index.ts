579da3dabf3cd3adafeef3f7295a26753c17e759
Nicholas Drake <hellolao8n@gmail.com>
2022-02-28 16:25:34 +0000

    make exports explicit in index

--- assignment/src/index.ts
+++ assignment/src/index.ts
@@ -6,8 +6,17 @@ export { TurnRoll, TurnFinish, TurnInJail, TurnOwnedProperty,
     TurnUnownedProperty} from './api/turn';
 
 // Types
-export { BoardLocation, Space } from './types/board';
-export { Money, GBP, EUR, USD } from "./types/money";
+export { Money, GBP, EUR, USD } from './types/money';
+export { Deed } from './types/space/deed';
+export { FreeParking } from './types/space/free_parking';
+export { Card } from './types/space/card';
+export { Go } from './types/space/go';
+export { GoToJail } from './types/space/gotojail';
+export { Jail } from './types/space/jail';
+export { Tax } from './types/space/tax';
+export { Train } from './types/space/train';
+export { Utility } from './types/space/utility';
+export { Space, BoardLocation, BoardStreet, BoardNumber } from './types/board';
 export { NumPlayers, PlayerID } from './types/player';
 export { Owner } from './types/ownership';
 export { PairDiceValue } from './types/dice';
